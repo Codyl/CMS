ee1c51316fe24c0a7bd051388862ddd8d82834d6
Cody Lillywhite <codylillyw@gmail.com>
2021-10-05 23:48:14 -0400

    Week 4: Add Document

--- src/app/documents/document-list/document-list.component.ts
+++ src/app/documents/document-list/document-list.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, EventEmitter, OnInit, Output } from '@angular/core';
+import Document from '../document.model';
 
 @Component({
   selector: 'app-document-list',
@@ -6,10 +7,51 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./document-list.component.css']
 })
 export class DocumentListComponent implements OnInit {
-
+  documents: Document[] = [
+      {
+        id: 1,
+        name: 'test1',
+        url: 'test.com',
+        description: 'A test of the app',
+        children: ''
+      },
+      {
+        id: 2,
+        name: 'test2',
+        url: 'test.com',
+        description: 'A test of the app',
+        children: ''
+      },
+      {
+        id: 3,
+        name: 'test3',
+        url: 'test.com',
+        description: 'A test of the app',
+        children: ''
+      },
+      {
+        id: 4,
+        name: 'test4',
+        url: 'test.com',
+        description: 'A test of the app',
+        children: ''
+      },
+      {
+        id: 5,
+        name: 'test5',
+        url: 'test.com',
+        description: 'A test of the app',
+        children: ''
+      }
+    ];
+  @Output() selectedDocumentEvent = new EventEmitter<Document>();
   constructor() { }
 
   ngOnInit(): void {
+    
+  }
+  onSelectedDocument(document: Document) {
+    this.selectedDocumentEvent.emit(document);
   }
 
 }
